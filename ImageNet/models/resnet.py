dad6a41bb170bba94f9818e7ba891163143c3e10
anonymous <xiao_yang@sjtu.edu.cn>
2020-12-09 11:22:09 +0800

    update

--- ImageNet/models/resnet.py
+++ ImageNet/models/resnet.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
-from .utils import load_state_dict_from_url
 import torch.nn.functional as F
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
